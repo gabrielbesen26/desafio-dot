d586d26f8e81aeed545ba40d4df93b9d42cdc877
gabriel <gabriel.besen26@gmail.com>
2021-05-13 16:07:30 -0300

    changing test target and updating files

--- cypress/integration/carrinho/CarrinhoTest.spec.js
+++ cypress/integration/carrinho/CarrinhoTest.spec.js
@@ -2,12 +2,34 @@
 
 describe('Teste na funcionalidade: Carrinho de Compras', () =>{
 
+    const _idProduto = 1;
+
     it('Clicando em comprar deve adicionar o item ao carrinho de compras', () => {
-        cy.visit('/produto/131037/iphone-se-branco-128gb-mhgu3br-a');
-        cy.title().should('equal', 'iPhone SE Branco, 128GB - MHGU3BR/A | KaBuM!');
-        cy.get('.botao-comprar').click();
-        cy.get('#compre_junto').should('contain.text', 'ESTE PRODUTO FOI ADICIONADO AO CARRINHO');
-        cy.visit('/carrinho');
+        cy.visit(`?id_product=${_idProduto}&controller=product`);
+        cy.title()
+            .should('contain', 'Faded Short Sleeve T-shirts');
+        cy.get('.shopping_cart')
+            .find('span')
+            .should('contain.text', '(empty)');
+        cy.get('#quantity_wanted')
+            .clear()
+            .type('1');
+        cy.get('#group_1')
+            .select('L');
+        cy.get('#color_to_pick_list')
+            .find('li')
+            .its(1)
+            .click();
+        cy.get('#add_to_cart')
+            .find('button')
+            .click();
+        cy.get('#layer_cart')
+            .should('be.visible')
+            .and('contain.text', 'Product successfully added to your shopping cart');
+        cy.visit('?controller=order');
+        cy.get('.shopping_cart')
+            .find('span')
+            .should('contain.text', '1 Product');
     })
 
 })
