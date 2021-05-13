85074ab56bb13d1c99707161c7241a726822d3b5
gabriel <gabriel.besen26@gmail.com>
2021-05-13 20:37:49 -0300

    Fix env file and test

--- cypress/integration/carrinho/CarrinhoTest.spec.js
+++ cypress/integration/carrinho/CarrinhoTest.spec.js
@@ -3,9 +3,10 @@
 describe('Teste na funcionalidade: Carrinho de Compras', () =>{
 
     const _idProduto = 1;
+    const _url = Cypress.env('QA_URL');
 
     it('Clicando em comprar deve adicionar o item ao carrinho de compras', () => {
-        cy.visit(`?id_product=${_idProduto}&controller=product`);
+        cy.visit(`${_url}/index.php?id_product=${_idProduto}&controller=product`);
         cy.title()
             .should('contain', 'Faded Short Sleeve T-shirts');
         cy.get('.shopping_cart')
@@ -26,10 +27,14 @@ describe('Teste na funcionalidade: Carrinho de Compras', () =>{
         cy.get('#layer_cart')
             .should('be.visible')
             .and('contain.text', 'Product successfully added to your shopping cart');
-        cy.visit('?controller=order');
-        cy.get('.shopping_cart')
-            .find('span')
-            .should('contain.text', '1 Product');
+        cy.visit(`${_url}/index.php?controller=order`);
+        cy.get('#cart_summary')
+            .find('tbody > tr')
+            .its('length')
+            .should('equal', 1);
+        cy.get('.cart_product > a > img')
+            .should('have.attr', 'src')
+            .and('equal', `${_url}/img/p/3/3-small_default.jpg`);
     })
 
 })
