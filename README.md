74da0fcf064e6a0647d265a3e6e39cb632bb5582
gabriel <gabriel.besen26@gmail.com>
2021-05-15 13:05:42 -0300

    Add steps and update readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# desafio-dot
-Repositório de testes automatizados para o desafio da Dot Digital Group
+# Desafio Dot - Analista de Testes Automatizados
+*Considerando um site de compras de varejo (americanas, pontofrio, extra entre outros), escolha um deles e crie alguns casos de teste considerando o cenário ‘carrinho de compras’. Em seguida, desenvolva os scrips de automação (na linguagem que preferir) dos casos de teste e disponibilize o link do seu github.*
+
+## Sobre
+**Site escolhido**: http://automationpractice.com/
+
+**Tecnologias**: JavaScript e Cypress
+
+**Requisitos**: 
+Node.Js 14.17.0
+Npm 6.14.13
+Git  2.30.2
+
+## Como executar?
+O repositório com os testes estão configurados somente para rodar localmente no momento.
+Para executá-los, basta clonar este repositório abrindo seu terminal, navegar até o diretório desejado e colar o comando:
+
+    git clone https://github.com/gabrielbesen26/desafio-dot
+Seguir do comando:
+
+    npm install
+Após a instalação das dependências, execute o comando:
+
+    npm run cy:run
+
+## Considerações
+Os testes:
+*Clicar em adicionar uma quantidade deve atualizar os valores*
+*Editar o campo quantidade deve atualizar os valores* 
+
+Me deparei com um comportamento diferente em comparação aos testes manuais. Foi implementado a espera das requisições http, porém, mesmo assim o problema persistiu. Essa é uma situação onde eu alinharia com algum desenvolvedor para investigarmos o que pode estar acontecendo com o componente de edição da quantidade dos itens no carrinho. 
+
+Qualquer dúvida estou a disposição.
+
+
--- cypress/integration/carrinho/CarrinhoTest.spec.js
+++ cypress/integration/carrinho/CarrinhoTest.spec.js
@@ -7,61 +7,46 @@ describe('Teste na funcionalidade: Carrinho de Compras', () =>{
 
     it('Clicando em comprar deve adicionar o item ao carrinho de compras', () => {
         cy.visit(`${_url}/index.php?id_product=${_idProduto}&controller=product`);
-        cy.title()
-            .should('contain', 'Faded Short Sleeve T-shirts');
-        cy.get('.shopping_cart')
-            .find('span')
-            .should('contain.text', '(empty)');
-        cy.get('#quantity_wanted')
-            .clear()
-            .type('1');
-        cy.get('#group_1')
-            .select('L');
-        cy.get('#color_to_pick_list')
-            .find('li')
-            .its(1)
-            .click();
-        cy.get('#add_to_cart')
-            .find('button')
-            .click();
-        cy.get('#layer_cart')
-            .should('be.visible')
+        cy.title().should('contain', 'Faded Short Sleeve T-shirts');
+        cy.get('.shopping_cart').find('span').should('contain.text', '(empty)');
+        cy.get('#quantity_wanted').clear().type('1');
+        cy.get('#group_1').select('L');
+        cy.get('#color_to_pick_list').find('li').its(1).click();
+        
+        cy.intercept({
+                method : 'POST',
+                url : `${_url}/*`
+            }).as('postProduto');
+        cy.intercept({
+            method : 'GET',
+            url : `${_url}/*`
+        }).as('getCarrinho');
+
+        cy.get('#add_to_cart').find('button').click();
+        cy.wait('@postProduto');
+        cy.get('#layer_cart').should('be.visible')
             .and('contain.text', 'Product successfully added to your shopping cart');
-        cy.visit(`${_url}/index.php?controller=order`);
-        cy.get('#cart_summary')
-            .find('tbody > tr')
-            .its('length')
+        cy.get('.button-container').contains('Proceed to checkout').click();
+        cy.wait('@getCarrinho');
+        
+        cy.get('#cart_summary').find('tbody > tr').its('length')
             .should('equal', 1);
-        cy.get('.cart_product > a > img')
-            .should('be.visible')
+        cy.get('.cart_product > a > img').should('be.visible')
             .and('have.attr', 'src')
             .and('equal', `${_url}/img/p/3/3-small_default.jpg`);
         cy.get('.cart_description')
             .should('contain.text', 'Faded Short Sleeve T-shirts')
             .and('contain.text', 'Color : Blue, Size : L');
-        cy.get('.cart_avail')
-            .should('contain.text', 'In stock');
-        cy.get('.cart_unit')
-            .should('contain.text', '$16.51');
-        cy.get('.cart_quantity.text-center')
-            .find('input')
-            .its(1)
+        cy.get('.cart_avail').should('contain.text', 'In stock');
+        cy.get('.cart_unit').should('contain.text', '$16.51');
+        cy.get('.cart_quantity.text-center').find('input').its(1)
             .should('have.value', 1);
-        cy.get('.cart_total')
-            .should('contain.text', '$16.51');
-        cy.get('.cart_total_price')
-            .its(0)
-            .should('contain.text', '$16.51');
-        cy.get('.cart_total_delivery')
-            .should('contain.text', '$2.00');
-        cy.get('.cart_total_price')
-            .its(1)
-            .should('contain.text', '$18.51');
-        cy.get('.cart_total_tax')
-            .should('contain.text', '$0.00');
-        cy.get('.cart_total_price')
-            .its(2)
-            .should('contain.text', '$18.51');
+        cy.get('.cart_total').should('contain.text', '$16.51');
+        cy.get('.cart_total_price').its(0).should('contain.text', '$16.51');
+        cy.get('.cart_total_delivery').should('contain.text', '$2.00');
+        cy.get('.cart_total_price').its(1).should('contain.text', '$18.51');
+        cy.get('.cart_total_tax').should('contain.text', '$0.00');
+        cy.get('.cart_total_price').its(2).should('contain.text', '$18.51');
     });
 
     it('Clicar em adicionar uma quantidade deve atualizar os valores', () => {
@@ -95,7 +80,8 @@ describe('Teste na funcionalidade: Carrinho de Compras', () =>{
             .find('input')
             .its(1)
             .clear()
-            .type('3{enter}');
+            .type('{backspace}3{enter}');
+        //BUG ENCONTRADO
         cy.get('.cart_total')
             .should('contain.text', '$49.53');
         cy.get('.cart_total_price')
@@ -112,5 +98,11 @@ describe('Teste na funcionalidade: Carrinho de Compras', () =>{
             .its(2)
             .should('contain.text', '$51.53');
     });
+
+    it('Clicar no botão excluir deve remover produto do carrinho', () => {
+        cy.get('.cart_quantity_delete').click();
+        cy.get('.alert.alert-warning').should('be.visible')
+            .and('contain.text', 'Your shopping cart is empty.');
+    })
 
 })
